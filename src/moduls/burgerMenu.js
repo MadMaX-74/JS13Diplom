1488ceba403d6d9e06e3577243f1fa95711a9e58
Maksim Maltcev <maximus3506@mail.ru>
2020-07-23 21:33:02 +0300

    закрепил бургер меню

--- src/moduls/burgerMenu.js
+++ src/moduls/burgerMenu.js
@@ -3,15 +3,18 @@ const burgerMenu = () => {
     const topMenu = document.querySelector('.top-menu');
     const topMenuBtn = topMenu.querySelector('.top-menu img');
     const popupMenu = document.querySelector('.popup-menu');
+    const windowInnerWidth = document.documentElement.clientWidth;
+
+    if (windowInnerWidth < 768) {
+        topMenu.style.position = 'fixed';
+    }
 
     topMenuBtn.addEventListener('click', () => {
         popupMenu.style.display = 'flex';
-
     });
 
     popupMenu.addEventListener('click', event => {
         const target = event.target;
-        console.log(target);
         if (target.matches('.popup-menu img') || target.matches('.popup-menu') || target.matches('.popup-menu li') || target.matches('.popup-menu a')) {
             popupMenu.style.display = 'none';
         }
