31213733e2b8c60f6054dc1cb804349a3ce6cab5
Maksim Maltcev <maximus3506@mail.ru>
2020-07-23 20:58:19 +0300

    сделал показ и работу бургер меню

--- src/moduls/burgerMenu.js
+++ src/moduls/burgerMenu.js
@@ -1,5 +1,22 @@
 
-const burgerMenu = () =>{
+const burgerMenu = () => {
+    const topMenu = document.querySelector('.top-menu');
+    const topMenuBtn = topMenu.querySelector('.top-menu img');
+    const popupMenu = document.querySelector('.popup-menu');
+
+    topMenuBtn.addEventListener('click', () => {
+        popupMenu.style.display = 'flex';
+
+    });
+
+    popupMenu.addEventListener('click', event => {
+        const target = event.target;
+        console.log(target);
+        if (target.matches('.popup-menu img') || target.matches('.popup-menu') || target.matches('.popup-menu li') || target.matches('.popup-menu a')) {
+            popupMenu.style.display = 'none';
+        }
+    });
+
 
 };
 
